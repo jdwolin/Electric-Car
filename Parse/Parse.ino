106392c705a391e0f2f16e12b2cb05e823dde377
jdwolin <jason@satisfiedinjesus.org>
2019-09-01 17:04:43 -0600

    Organizing

---
+++ Parse/Parse.ino
@@ -0,0 +1,45 @@
+// simple parse demo
+//char receivedChars[] = "IC 1, C1:3.7654, C2:3.4555, C3:3.5887, C4:3.4433, C5:3.8774, C6:3.9454, C7:3.3434, C8:3.3434, C9:3.3434, C10:3.3343, C11:3.2324, C12:3.3434, C13:3.3434, C14:3.2322, C15:3.3443, C16:3.3434, C17:3.3434, C18:3.3493";
+/*
+ StringSplitter example: Basic
+
+ This example shows how to split a delimited string to substrings (items) by using the StringSplitter library.
+ The output is visible in the Serial Monitor.
+
+ For more details see: https://github.com/aharshac/StringSplitter
+*/
+/*
+ StringSplitter example: Basic
+
+ This example shows how to split a delimited string to substrings (items) by using the StringSplitter library.
+ The output is visible in the Serial Monitor.
+
+ For more details see: https://github.com/aharshac/StringSplitter
+*/
+
+//#include "StringSplitter.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("StringSplitter Library Test");
+
+  //String strTest = "123,456,789,abc";
+  //Serial.println(strTest);
+/*
+  StringSplitter *splitter = new StringSplitter(strTest, ',', 3);  // new StringSplitter(string_to_split, delimiter, limit)
+  int itemCount = splitter->getItemCount();
+  Serial.println("Item count: " + Scopytring(itemCount));
+
+  for(int i = 0; i < itemCount; i++){
+    String item = splitter->getItemAtIndex(i);
+    Serial.println("Item @ index " + Stricopy file name
+    ng(i) + ": " + String(item));
+  }
+*/
+  Serial.println("End of program.");
+  Serial.end();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
